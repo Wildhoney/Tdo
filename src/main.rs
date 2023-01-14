3d22acbd146ef66754ef65f98fd4cc8075fa18ff
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-14 17:59:50 +0000

    Added SQLite DB for writing todos

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+mod actions;
 mod cli;
 mod renderer;
 mod types;
+mod utils;
 
 pub fn main() {
     let output = cli::run();
---
+++ src/utils/mod.rs
@@ -0,0 +1,41 @@
+use rusqlite::Connection;
+
+use crate::types::Task;
+
+pub fn write_todo(task: Task) -> Option<Task> {
+    let connection = Connection::open("tdo.db").unwrap();
+
+    connection
+        .execute(
+            "CREATE TABLE IF NOT EXISTS tasks (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            description TEXT NOT NULL,
+            completed   BOOL NOT NULL
+        )",
+            (),
+        )
+        .ok()?;
+
+    connection
+        .execute(
+            "INSERT INTO tasks (description, completed) VALUES (?1, ?2)",
+            (&task.description, &task.completed),
+        )
+        .ok()?;
+
+    let mut statement = connection
+        .prepare("SELECT * FROM tasks WHERE id = ?1")
+        .ok()?;
+
+    let mut tasks = statement
+        .query_map([connection.last_insert_rowid()], |row| {
+            Ok(Task {
+                id: Some(row.get(0)?),
+                description: row.get(1)?,
+                completed: row.get(2)?,
+            })
+        })
+        .ok()?;
+
+    tasks.next().unwrap().ok()
+}
