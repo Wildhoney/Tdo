381a0c8a97a9e2907ff862aa1325851081f0e0cc
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-14 19:33:10 +0000

    Added better (but still primitive) output

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,5 +1,16 @@
-use crate::types::Output;
+use crate::types::{Output, Task};
 
-pub fn print(output: Output) {
-    println!("Hii! {:?}", output)
+pub fn print(output: Output) -> () {
+    match output {
+        Output::Add(Some(task)) => println!("Added: {:?}", task),
+        Output::Remove(Some(task)) => println!("Removed: {:?}", task),
+        Output::List(Some(tasks)) => list(tasks),
+        _ => println!("There appears to be a problem."),
+    }
+}
+
+fn list(tasks: Vec<Task>) -> () {
+    for task in tasks {
+        println!("{}: {}", task.id.unwrap_or(0), task.description);
+    }
 }
