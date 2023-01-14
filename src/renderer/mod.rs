77c2ae5122d1374b158e548c430148e0a27d2e9b
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-14 19:34:31 +0000

    Added add and remove outputs

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -2,11 +2,19 @@ use crate::types::{Output, Task};
 
 pub fn print(output: Output) -> () {
     match output {
-        Output::Add(Some(task)) => println!("Added: {:?}", task),
-        Output::Remove(Some(task)) => println!("Removed: {:?}", task),
+        Output::Add(Some(task)) => add(task),
+        Output::Remove(Some(task)) => remove(task),
         Output::List(Some(tasks)) => list(tasks),
         _ => println!("There appears to be a problem."),
     }
+}
+
+fn add(task: Task) -> () {
+    println!("Added: {:?}", task)
+}
+
+fn remove(task: Task) -> () {
+    println!("Removed: {:?}", task)
 }
 
 fn list(tasks: Vec<Task>) -> () {
