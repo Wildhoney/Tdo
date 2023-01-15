24538b1ed5d0ae1f5f24474a1671e25e22244f4f
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-15 18:32:38 +0000

    Added green tick to completed todos

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -6,23 +6,24 @@ pub fn get_symbols() -> Symbols {
     Symbols {
         dot: ".".dimmed().to_string(),
         bullet: "◦".dimmed().to_string(),
+        tick: "✓".bright_green().to_string(),
     }
 }
 
 pub fn get_percentage_emoji(completed_percentage: f64) -> String {
-    if completed_percentage >= 0.0 && completed_percentage <= 0.1 {
+    if completed_percentage >= 0.0 && completed_percentage <= 10.0 {
         return "😭".to_string();
     }
 
-    if completed_percentage >= 0.1 && completed_percentage <= 0.25 {
+    if completed_percentage >= 10.0 && completed_percentage <= 25.0 {
         return "😣".to_string();
     }
 
-    if completed_percentage >= 0.25 && completed_percentage <= 0.5 {
+    if completed_percentage >= 25.0 && completed_percentage <= 50.0 {
         return "😞".to_string();
     }
 
-    if completed_percentage >= 0.5 && completed_percentage <= 0.75 {
+    if completed_percentage >= 50.0 && completed_percentage <= 75.0 {
         return "🥹".to_string();
     }
 
