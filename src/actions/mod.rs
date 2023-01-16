2cd839b6658e34eafbba14f4f420189197488e4c
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-16 13:11:28 +0000

    Moved DB connection code

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,28 +1,32 @@
 use crate::{
     db::{add_todo, edit_todo, get_all_todos, get_todays_todos, get_todo, remove_todo},
-    types::Task,
+    types::{DbFile, Task},
 };
 
 pub fn add(description: &str) -> Option<Task> {
     let task = Task::new(description.to_string());
-    add_todo(task)
+    DbFile::new().and_then(|db| add_todo(&db, task))
 }
 
 pub fn remove(id: usize) -> Option<Task> {
-    let task = get_todo(id)?;
-    remove_todo(task)
+    DbFile::new().and_then(|db| {
+        let task = get_todo(&db, id)?;
+        remove_todo(&db, task)
+    })
 }
 
 pub fn edit(id: usize, description: Option<&String>, completed: Option<bool>) -> Option<Task> {
-    let mut task = get_todo(id)?;
-    task.description = description.unwrap_or(&task.description).to_owned();
-    task.completed = completed.unwrap_or(task.completed).to_owned();
-    edit_todo(task)
+    DbFile::new().and_then(|db| {
+        let mut task = get_todo(&db, id)?;
+        task.description = description.unwrap_or(&task.description).to_owned();
+        task.completed = completed.unwrap_or(task.completed).to_owned();
+        edit_todo(&db, task)
+    })
 }
 
 pub fn list(all: Option<bool>) -> Option<Vec<Task>> {
-    match all {
-        Some(true) => get_all_todos(),
-        _ => get_todays_todos(),
-    }
+    DbFile::new().and_then(|db| match all {
+        Some(true) => get_all_todos(&db),
+        _ => get_todays_todos(&db),
+    })
 }
--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,31 +1,10 @@
 use chrono::{NaiveDateTime, NaiveTime, Utc};
 use rusqlite::{Connection, Params};
 
-use crate::types::Task;
+use crate::{types::Task, utils::get_db_connection};
 
-pub const DB_FILENAME: &str = "tdo.db";
-
-fn get_db_connection() -> Option<Connection> {
-    let connection = Connection::open(DB_FILENAME).ok()?;
-
-    connection
-        .execute(
-            "CREATE TABLE IF NOT EXISTS tasks (
-            id             INTEGER PRIMARY KEY AUTOINCREMENT,
-            description    TEXT NOT NULL,
-            completed      BOOL NOT NULL,
-            date_added     DATETIME DEFAULT CURRENT_TIMESTAMP,
-            date_modified  DATETIME DEFAULT CURRENT_TIMESTAMP
-        )",
-            (),
-        )
-        .ok()?;
-
-    Some(connection)
-}
-
-pub fn add_todo(task: Task) -> Option<Task> {
-    let db = get_db_connection()?;
+pub fn add_todo(db: &Connection, task: Task) -> Option<Task> {
+    let db = get_db_connection(db)?;
 
     db.execute(
         "INSERT INTO tasks (description, completed) VALUES (?1, ?2)",
@@ -39,15 +18,15 @@ pub fn add_todo(task: Task) -> Option<Task> {
         .ok()?
 }
 
-pub fn remove_todo(task: Task) -> Option<Task> {
-    let db = get_db_connection()?;
+pub fn remove_todo(db: &Connection, task: Task) -> Option<Task> {
+    let db = get_db_connection(db)?;
     db.execute("DELETE FROM tasks WHERE id = ?1", [&task.id.unwrap_or(0)])
         .ok()?;
     Some(task)
 }
 
-pub fn edit_todo(task: Task) -> Option<Task> {
-    let db = get_db_connection()?;
+pub fn edit_todo(db: &Connection, task: Task) -> Option<Task> {
+    let db = get_db_connection(db)?;
     db.execute(
         "UPDATE tasks SET description = ?1, completed = ?2, date_modified = CURRENT_TIMESTAMP WHERE id = ?3",
         (
@@ -60,24 +39,25 @@ pub fn edit_todo(task: Task) -> Option<Task> {
     Some(task)
 }
 
-pub fn get_todays_todos() -> Option<Vec<Task>> {
+pub fn get_todays_todos(db: &Connection) -> Option<Vec<Task>> {
     let time = NaiveTime::from_hms_milli_opt(0, 0, 0, 0).unwrap();
     let today = NaiveDateTime::new(Utc::now().date_naive(), time).to_string();
     prepare_todos(
+        db,
         &"SELECT * FROM tasks WHERE date_added >= ?1".to_string(),
         [today],
     )
 }
 
-pub fn get_all_todos() -> Option<Vec<Task>> {
-    prepare_todos(&"SELECT * FROM tasks".to_string(), [])
+pub fn get_all_todos(db: &Connection) -> Option<Vec<Task>> {
+    prepare_todos(db, &"SELECT * FROM tasks".to_string(), [])
 }
 
-fn prepare_todos<P>(query: &String, params: P) -> Option<Vec<Task>>
+fn prepare_todos<P>(db: &Connection, query: &String, params: P) -> Option<Vec<Task>>
 where
     P: Params,
 {
-    let db = get_db_connection()?;
+    let db = get_db_connection(db)?;
     let mut statement = db.prepare(query).ok()?;
     let query = statement
         .query_map(params, |row| Ok(Task::from_db(row)))
@@ -92,10 +72,42 @@ where
     }
 }
 
-pub fn get_todo(id: usize) -> Option<Task> {
-    let db = get_db_connection()?;
-    let mut statement = db.prepare("SELECT * FROM tasks WHERE id >= ?1").ok()?;
+pub fn get_todo(db: &Connection, id: usize) -> Option<Task> {
+    let db = get_db_connection(db)?;
+    let mut statement = db.prepare("SELECT * FROM tasks WHERE id = ?1").ok()?;
     statement
         .query_row([id], |row| Ok(Task::from_db(row)))
         .ok()?
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        db::{add_todo, edit_todo, get_todays_todos, remove_todo},
+        types::{DbMemory, Task},
+    };
+
+    #[test]
+    fn it_can_manage_todos() {
+        let db = DbMemory::new().unwrap();
+
+        let task_from_add = add_todo(&db, Task::new("I am Imogen!".to_string()));
+        assert!(task_from_add.is_some());
+
+        let tasks_from_today = get_todays_todos(&db);
+        assert!(tasks_from_today.is_some());
+        assert_eq!(tasks_from_today.unwrap().len(), 1);
+
+        let mut task_for_edit = task_from_add.clone().unwrap();
+        task_for_edit.description = "I am Adam!".to_string();
+        let task_from_edit = edit_todo(&db, task_for_edit);
+        assert!(task_from_edit.is_some());
+        assert_eq!(task_from_edit.unwrap().description, "I am Adam!");
+
+        let remove = remove_todo(&db, task_from_add.unwrap());
+        assert!(remove.is_some());
+
+        let todays_todos = get_todays_todos(&db);
+        assert!(todays_todos.is_none());
+    }
 }
