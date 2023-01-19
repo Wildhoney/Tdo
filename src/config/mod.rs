02ec37c301248321d569280c435f1de16d8131fd
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-19 12:06:20 +0000

    Moved DB location into config directory

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,9 @@
+use std::{fs::create_dir_all, path::MAIN_SEPARATOR};
+
+use dirs::config_dir;
+
 pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 pub const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-pub const DB_PATH: &str = "tdo.db";
 
 pub const CMD_ADD: &str = "add";
 pub const CMD_REMOVE: &str = "remove";
@@ -9,3 +11,23 @@ pub const CMD_EDIT: &str = "edit";
 pub const CMD_LIST: &str = "list";
 pub const CMD_MARK: &str = "mark";
 pub const CMD_DATABASE: &str = "database";
+
+lazy_static! {
+    pub static ref DB_PATH: String = {
+        let separator = MAIN_SEPARATOR;
+
+        config_dir()
+            .map(|dir| {
+                let path = format!(
+                    "{}{separator}Tdo",
+                    dir.into_os_string().to_str().unwrap_or(""),
+                );
+
+                match create_dir_all(&path) {
+                    Ok(_) => format!("{path}{separator}tdo.db"),
+                    Err(_) => "tdo.db".to_string(),
+                }
+            })
+            .unwrap()
+    };
+}
