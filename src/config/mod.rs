927659f4e8706b098a15abb82652bdf44ca2396a
Adam Timberlake <adam.timberlake@clearscore.com>
2023-01-19 11:15:48 +0000

    Simplifed the 'edit' command

---
+++ src/config/mod.rs
@@ -0,0 +1,11 @@
+pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+pub const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub const DB_PATH: &str = "tdo.db";
+
+pub const CMD_ADD: &str = "add";
+pub const CMD_REMOVE: &str = "remove";
+pub const CMD_EDIT: &str = "edit";
+pub const CMD_LIST: &str = "list";
+pub const CMD_MARK: &str = "mark";
+pub const CMD_DATABASE: &str = "database";
